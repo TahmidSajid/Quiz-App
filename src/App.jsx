5dc692ae584e8dc5566788f02d83729540fe5375
TahmidSajid <tahmidkhansajid@gmail.com>
2026-05-15 22:03:33 +0600

    Code quality upgraded

--- src/App.jsx
+++ src/App.jsx
@@ -60,9 +60,9 @@ function App() {
       <Container className="vh-100 d-flex">
         <Row className="justify-content-center align-items-center w-100">
           <Col className="col-lg-6">
-            {screen == "start" && <StartScreen handleStart={handleStart} />}
-            {screen == "on-going" && <QuestionScreen handleSubmit={handleSubmit} question={question} questionCount={questions.length} answerCount={currentIndex}/>}
-            {screen == "end" && <ResultScreen handleReset={handleReset} rightCount={rightCount} wrongCount={wrongCount} answers={answers}/>}
+            {screen === "start" && <StartScreen handleStart={handleStart} />}
+            {screen === "on-going" && <QuestionScreen handleSubmit={handleSubmit} question={question} questionCount={questions.length} answerCount={currentIndex}/>}
+            {screen === "end" && <ResultScreen handleReset={handleReset} rightCount={rightCount} wrongCount={wrongCount} answers={answers}/>}
           </Col>
         </Row>
       </Container>
